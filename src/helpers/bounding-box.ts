5be50aa526a58bba4cf03350b46449188ab5953a
thiagorf <trfarciroli@gmail.com>
2023-05-04 14:29:32 -0300

    add bounding box helper

---
+++ src/helpers/bounding-box.ts
@@ -0,0 +1,26 @@
+import { PolyInfo } from "../App";
+
+type BoundingBoxCoordinates = Pick<PolyInfo, "positions">;
+
+export function boundingBox(
+  coordinates: BoundingBoxCoordinates
+): [number, number][] {
+  const lat: number[] = [],
+    long: number[] = [];
+
+  coordinates.positions.forEach((x) => (lat.push(x[0]), long.push(x[1])));
+
+  const minLat = Math.min(...lat);
+  const maxLat = Math.max(...lat);
+  const minLong = Math.min(...long);
+  const maxLong = Math.max(...long);
+
+  // required coordinates: [minLat, maxLog], [maxLat, minLong]
+
+  return [
+    [minLat, maxLong],
+    [maxLat, maxLong],
+    [minLat, minLong],
+    [maxLat, minLong],
+  ];
+}
