52e2c2f6ca8fed57e6a655c93acbf73da2834975
thiagorf <trfarciroli@gmail.com>
2023-05-23 14:50:10 -0300

    refactoring bearing helper

--- src/helpers/bearing.ts
+++ src/helpers/bearing.ts
@@ -1,19 +1,25 @@
+import { toDegrees } from "./to-degrees";
+import { toRad } from "./to-radians";
+
 type BearingAngleCoodinates = {
   startPoint: number[];
   endPoint: number[];
 };
 
 export function getBearing({ startPoint, endPoint }: BearingAngleCoodinates) {
-  const y = Math.sin(endPoint[1] - startPoint[1]) * Math.cos(endPoint[0]);
+  const startLat = toRad(startPoint[0]);
+  const startLong = toRad(startPoint[1]);
+
+  const endLat = toRad(endPoint[0]);
+  const endLong = toRad(endPoint[1]);
+
+  const y = Math.sin(endLong - startLong) * Math.cos(endLat);
   const x =
-    Math.cos(startPoint[0]) * Math.sin(endPoint[0]) -
-    Math.sin(startPoint[0]) *
-      Math.cos(endPoint[0]) *
-      Math.cos(endPoint[1] - startPoint[1]);
+    Math.cos(startLat) * Math.sin(endLat) -
+    Math.sin(startLat) * Math.cos(endLat) * Math.cos(endLong - startLong);
 
   const o = Math.atan2(y, x);
-
-  const bearing = ((o * 180) / Math.PI + 360) % 360;
+  const bearing = (toDegrees(o) + 360) % 360;
 
   return bearing;
 }
